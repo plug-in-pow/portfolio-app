a57d46f41762735a94247b7fd25575b8485381cc
plug-in-pow <tripathimayank36@gmail.com>
2022-10-29 19:37:02 +0530

    added animation to navbar and updated css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('activeInactive', [
+      state('active', style({
+        background: 'blueviolet',
+        color: 'white',
+        boxShadow: 'rgb(223 0 255 / 25%) 0px 8px 24px'
+      })),
+      state('inactive', style({
+        background: 'transparent',
+        color: 'black',
+        boxShadow: 'none'
+      })),
+      transition('active => inactive', [
+        animate('200ms cubic-bezier(0.61, 1, 0.88, 1)')
+      ]),
+      transition('inactive => active', [
+        animate('200ms cubic-bezier(0.37, 0, 0.63, 1)')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
+  navbarData: any[] = [
+    { "name": "Home", "status": "active" },
+    { "name": "About", "status": "inactive" },
+    { "name": "Skills", "status": "inactive" },
+    { "name": "Projects", "status": "inactive" },
+  ];
 
-  
-
+  navbarItemClick(event: Event) : void {
+    let clickedItem: string = (event.target as HTMLInputElement).innerHTML;
+    this.navbarData.forEach(obj => {
+      if(obj.name === clickedItem) {
+        obj.status = "active";
+      }else{
+        obj.status = "inactive";
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { PortfolioDashboardModule } from './portfolio-dashboard/portfolio-dashboard.module';
@@ -10,6 +11,7 @@ import { PortfolioDashboardModule } from './portfolio-dashboard/portfolio-dashbo
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     PortfolioDashboardModule
   ],
   providers: [],
